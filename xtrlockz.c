71c47b4267acb5b5b8a28b4e09362182bea69bb8
jibi <jibi@paranoici.org>
2014-02-28 00:38:10 +0100

    Restore license header

--- xtrlockz.c
+++ xtrlockz.c
@@ -1,3 +1,20 @@
+/*
+ * X Transparent Lock
+ *
+ * Copyright (C)1993,1994 Ian Jackson
+ * Copyright (C)2014 Jibi
+ *
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
